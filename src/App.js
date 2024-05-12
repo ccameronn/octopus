6dfb68d6335a45d46882a46c8feef05bed0279e4
Catherine Anozie <nayaanozie@gmail.com>
2024-05-12 07:31:21 +0100

    Leaderboard structure complete

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Score from "./components/Score/Score";
-import Zapped from "./components/Zapped/Zapped";
-import LeaderBoard from "./components/LeaderBoard/LeaderBoard";
+import Home from "./pages/HomePage/HomePage";
 
 function App() {
   return (
