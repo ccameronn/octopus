e505ee6ed736b373bd14d088dcc468eca475b9c2
Catherine Anozie <nayaanozie@gmail.com>
2024-05-12 07:31:21 +0100

    Leaderboard structure complete

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/HomePage/HomePage";
+import LeaderBoard from "./components/LeaderBoard/LeaderBoard";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <LeaderBoard />
+      <Routes></Routes>
     </BrowserRouter>
+
+    // <BrowserRouter>
+    //   <div className="container">
+    //     <Header />
+    //     <main>
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //       </Routes>
+    //     </main>
+    //     <Footer />
+    //   </div>
+    // </BrowserRouter>
   );
 }
 
