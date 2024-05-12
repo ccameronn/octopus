ac532d2c278ce756bf7cc68c45a9b5dc162f46e1
Catherine Anozie <nayaanozie@gmail.com>
2024-05-12 23:29:26 +0100

    leaderboard feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.scss";
 import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import HomePage from "./pages/HomePage/HomePage";
 
 import Header from "./components/Header/Header";
@@ -11,22 +10,20 @@ import LeaderBoard from "./components/LeaderBoard/LeaderBoard";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <main>
-    //     <Routes>
-    //       <Route path="/" element={<HomePage />} />
-    //     </Routes>
-    //   </main>
-    // </BrowserRouter>
-
-    <div className="page-container">
-      <Header />
-      <main>
-        <Score />
-      </main>
-      <Footer className="footer" />
-    </div>
+    <BrowserRouter>
+      <div className="page-container">
+        <Header />
+        <main>
+          <Score />
+          <LeaderBoard />
+          <Routes></Routes>
+        </main>
+        <Footer className="footer" />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+<LeaderBoard />;
